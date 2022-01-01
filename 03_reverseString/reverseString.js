6c801aa7a2d807e689649ca61d31fa98e2018a15
Richard Pignatiello <rpignatiello31@gmail.com>
2021-12-31 16:42:05 -0800

    finished exercised 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  let string = ""
+  for(let i = str.length - 1; i >= 0; i--)
+  {
+    string += str.charAt(i);
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+  let removed = arguments[0];
+  let values = Array.from(arguments);
+  for(let i = 0; i < removed.length; i++)
+  {
+    if(values.includes(removed[i]))
+    {
+      removed.splice(i, 1);
+      i--;
+    }
+  }
+  return removed;
 };
 
 // Do not edit below this line
